32c26cc1e169300a45d3bda355666f96db904c1f
Murtaza Tinwala <Murtaza.Tinwala@Teletracnavman.com>
2021-11-12 00:56:17 +1300

    initial commit - call REST apis and get rang and divisor info

---
+++ DivisorConsoleApp/DivisorConsoleApp/Models.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DivisorConsoleApp
+{
+    public class RangeInfo
+    {
+        public int lower { get; set; }
+        public int upper { get; set; }
+    }
+
+    public class OutputDetail
+    {
+        public int divisor { get; set; }
+        public string output { get; set; }
+    }
+
+    public class DivisorInfo
+    {
+        public OutputDetail[] outputDetails { get; set; }
+    }
+}
---
+++ DivisorConsoleApp/DivisorConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DivisorConsoleApp
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            var rangeInfo = await GetRangeInfo();
+            var divisorInfo = await GetDivisorInfo();
+
+            Console.WriteLine(rangeInfo.lower);
+            Console.WriteLine(rangeInfo.upper);
+
+            if (divisorInfo != null)
+            {
+                foreach (OutputDetail detail in divisorInfo.outputDetails) 
+                {
+                    Console.WriteLine($"divisor {detail.divisor}, output {detail.output}");
+                }
+            }
+        }
+
+        private static async Task<RangeInfo> GetRangeInfo()
+        {
+            var streamTask = client.GetStreamAsync("https://join.reckon.com/test1/rangeInfo");
+            var rangeInfo = await JsonSerializer.DeserializeAsync<RangeInfo>(await streamTask);
+            return rangeInfo;
+        }
+
+        private static async Task<DivisorInfo> GetDivisorInfo()
+        {
+            var streamTask = client.GetStreamAsync("https://join.reckon.com/test1/divisorInfo");
+            var divisorInfo = await JsonSerializer.DeserializeAsync<DivisorInfo>(await streamTask);
+            return divisorInfo;
+        }
+    }
+}
