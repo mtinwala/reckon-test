c5bc718d4e1716f0f2278c234f390483f5ded9fe
Murtaza Tinwala <Murtaza.Tinwala@Teletracnavman.com>
2021-11-12 01:24:46 +1300

    main logic

--- DivisorConsoleApp/DivisorConsoleApp/Program.cs
+++ DivisorConsoleApp/DivisorConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DivisorConsoleApp
@@ -10,36 +11,76 @@ namespace DivisorConsoleApp
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
+        private const int RETRY_COUNT = 5;
 
         static async Task Main(string[] args)
         {
             var rangeInfo = await GetRangeInfo();
             var divisorInfo = await GetDivisorInfo();
-
-            Console.WriteLine(rangeInfo.lower);
-            Console.WriteLine(rangeInfo.upper);
-
-            if (divisorInfo != null)
+            
+            if (rangeInfo != null && divisorInfo != null)
             {
-                foreach (OutputDetail detail in divisorInfo.outputDetails) 
+                for (int i = rangeInfo.lower; i <= rangeInfo.upper; i++) 
                 {
-                    Console.WriteLine($"divisor {detail.divisor}, output {detail.output}");
+                    string result = string.Empty;
+                    foreach (OutputDetail detail in divisorInfo.outputDetails)
+                    {
+                        if (i % detail.divisor == 0)
+                            result += detail.output;
+                    }
+                    Console.WriteLine($"{i}:{result}");
                 }
             }
         }
 
         private static async Task<RangeInfo> GetRangeInfo()
         {
-            var streamTask = client.GetStreamAsync("https://join.reckon.com/test1/rangeInfo");
-            var rangeInfo = await JsonSerializer.DeserializeAsync<RangeInfo>(await streamTask);
+            int attempts = RETRY_COUNT;
+            RangeInfo rangeInfo = null;
+
+            while (true)
+            {
+                try
+                {
+                    var streamTask = client.GetStreamAsync("https://join.reckon.com/test1/rangeInfo");
+                    rangeInfo = await JsonSerializer.DeserializeAsync<RangeInfo>(await streamTask);
+                    break;
+                }
+                catch
+                {
+                    if (--attempts == 0)
+                        throw;
+                    Thread.Sleep(1000);
+                }
+            }
+            
             return rangeInfo;
         }
 
         private static async Task<DivisorInfo> GetDivisorInfo()
         {
-            var streamTask = client.GetStreamAsync("https://join.reckon.com/test1/divisorInfo");
-            var divisorInfo = await JsonSerializer.DeserializeAsync<DivisorInfo>(await streamTask);
+            int attempts = RETRY_COUNT;
+            DivisorInfo divisorInfo = null;
+
+            while (true)
+            {
+                try
+                {
+                    var streamTask = client.GetStreamAsync("https://join.reckon.com/test1/divisorInfo");
+                    divisorInfo = await JsonSerializer.DeserializeAsync<DivisorInfo>(await streamTask);
+                    break;
+                }
+                catch
+                {
+                    if (--attempts == 0)
+                        throw;
+                    Thread.Sleep(1000);
+                }
+            }
+
             return divisorInfo;
         }
+
+        
     }
 }
